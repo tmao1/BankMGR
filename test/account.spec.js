9253b0ff7aa7c7ee09e96fbf9247f047396aad8a
John Zhang and Tony Mao <pair+john+tony@allstate.com>
2016-06-16 13:56:04 -0500

    push bank

--- test/account.spec.js
+++ test/account.spec.js
@@ -1,29 +1,35 @@
+/* eslint-disable prefer-arrow-callback, func-names,
+space-before-function-paren, no-traiing-spaces */
+
 const expect = require('chai').expect;
 const Account = require('../lib/account');
 
-const balance = require('../lib/accountBalance');
-
-describe('Account',function(){
-  describe('#account',function(){
-    it('should create a new account',function(){
-      let newAccount1 = new Account('Tony', 100);
+describe('Account', function() {
+  describe('#account', function() {
+    it('should create a new account', function() {
+      const newAccount1 = new Account('Tony');
       expect(newAccount1).to.be.instanceof(Account);
       expect(newAccount1.number).to.be.length(36);
-      expect(newAccount1.balance).to.equal(100);
-      expect(newAccount1.name).to.equal("Tony");
-      expect(newAccount1.deposits).to.be.length(0);
-      expect(newAccount1.withdraw).to.be.length(0);
+      expect(newAccount1.balance).to.equal(0);
+      expect(newAccount1.name).to.equal('Tony');
     });
+  });
 
-    it('should get the balance of the account',function(){
-
-      let newAccount = new Account('Tony', 1000);
-      const deposits = [50, 100, 500, 1000];
-      const withdraw = [40, 90, 100];
-
-      let balance = getBalance(deposits, withdraw);
-      expect(balance).to.equal(2650);
+  describe('#deposit', function() {
+    it('should return balance after deposit', function() {
+      const newAccount2 = new Account('Tony');
+      expect(newAccount2.deposit(400)).to.equal(400);
+      expect(newAccount2.deposit(200)).to.equal(600);
     });
+  });
 
+  describe('#withdraw', function(){
+    it('should return balance after withdraw', function() {
+      let newAccount3 = new Account('Tony');
+      newAccount3.deposit(400);
+      expect(newAccount3.withdraw(300)).to.equal(100);
+      expect(newAccount3.balance).to.equal(100);
+      expect(newAccount3.withdraw(500)).to.equal(100);
+    });
   });
 });
---
+++ test/bank.spec.js
@@ -0,0 +1,39 @@
+/* eslint-disable prefer-arrow-callback, func-names,
+space-before-function-paren, no-traiing-spaces */
+
+const expect = require('chai').expect;
+const Account = require('../lib/account');
+const Bank = require('../lib/bank');
+
+describe('Bank', function() {
+  describe('#bank', function() {
+    it('should create a new bank', function() {
+      const b1 = new Bank('Tony & John Bank');
+      expect(b1).to.be.instanceof(Bank);
+      //expect(b1.name).to.eqaul('Tony & John Bank');
+    });
+  });
+
+  describe('#addAccounts', function() {
+    it('should add an account to the bank', function() {
+      const b2 = new Bank('Tony & John Bank');
+      const a1 = new Account('a1');
+      b2.addAccount(a1);
+      expect(b2.accounts[0].name).to.equal('a1');
+    });
+  });
+
+  describe('#getValue', function() {
+    it('should get the value of the bank', function() {
+      const b3 = new Bank('Tony & John Bank');
+      const a1 = new Account('a1');
+      const a2 = new Account('a2');
+      b3.addAccount(a1);
+      b3.addAccount(a2);
+      a1.deposit(100);
+      a2.deposit(200);
+      expect(b3.getValue()).to.equal(300);
+    });
+  });
+
+});
